642ba956b58f4a50e518f2a54674c78eb42913d3
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-04-25 22:42:28 -0300

    Eu amo números

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+double numero;
+
+Console.Write("Digite um número: ");
+numero = Convert.ToDouble(Console.ReadLine());
+
+if (numero > 0)
+{
+ Console.ForegroundColor = ConsoleColor.Green;
+ Console.WriteLine("Positivo");
+}
+if (numero < 0)
+{
+Console.ForegroundColor = ConsoleColor.Red;
+ Console.WriteLine("Negativo");
+}
+if (numero == 0)
+{
+Console.ForegroundColor = ConsoleColor.Green;
+ Console.WriteLine("Zero");
+}
+
+Console.ResetColor();
